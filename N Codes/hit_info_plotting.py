31ff69f2e7b62833d550507a2b144eb63b43abce
JohannesKasselman <157305936+JohannesKasselman@users.noreply.github.com>
2024-12-15 16:48:28 +0000

    Changing File Name to My System

--- N Codes/hit_info_plotting.py
+++ N Codes/hit_info_plotting.py
@@ -2,7 +2,7 @@ import uproot
 import numpy
 import matplotlib.pyplot as plt
 
-file = uproot.open("/root/Mu3eProject/RawData/HitData/signal1_1_1944629_execution_1_run_num_836827_sort.root")
+file = uproot.open(r"C:\Users\m4joh\OneDrive\University\Mu3e\Raw Simulation Data\signal1_1_1944629_execution_1_run_num_836827_sort.root")
 
 keys = file.keys() # Accessing the file keys to see what info there is
 #print("Keys in the ROOT file:", keys)
