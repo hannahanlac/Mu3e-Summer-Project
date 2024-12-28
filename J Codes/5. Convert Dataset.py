56d86cc0d6b7a2738d0e1f5dcae40ba06e2ab95b
JohannesKasselman <157305936+JohannesKasselman@users.noreply.github.com>
2024-12-28 22:30:39 +0000

    Modifying Dataset Function

--- J Codes/5. Convert Dataset.py
+++ J Codes/5. Convert Dataset.py
@@ -8,7 +8,7 @@ import matplotlib.pyplot as plt
 
 
 #loading original dataset, extract classes we need, cut off particles by set up eta standard and sort by pT (Transverse Momentum) value
-def Dataset(signal_dir,eta_std):
+def Dataset(signal_dir):
     '''signal_dir: Path to signal data (particles of interest).
     bkg_dir: Path to background data (irrelevant or noise particles).
     eta_std: Maximum allowed pseudorapidity value.'''
@@ -18,35 +18,46 @@ def Dataset(signal_dir,eta_std):
 
     #extract spatial (layer, station, ladder, chip, pixel x, pixel y) classes from original dataset
     signal_branch = signal['Delphes;1']
-    phi_array_1 = signal_branch['Particle/Particle.Phi'].array(library="ak")
-    eta_array_1 = signal_branch['Particle/Particle.Eta'].array(library="ak")
-    pT_array_1 = signal_branch['Particle/Particle.PT'].array(library="ak")
+    # Check this, I don't think csv is that complicated (i.e., multi-layered), need specify
+    frame_array = signal_branch['Particle/Particle.PT'].array(library="ak")
+    pixelx_array = signal_branch['Particle/Particle.Pixel x'].array(library="ak")
+    pixely_array = signal_branch['Particle/Particle.Pixel y'].array(library="ak")
+    layer_array = signal_branch['Particle/Particle.Layer'].array(library="ak")
+    station_array = signal_branch['Particle/Particle.Station'].array(library="ak")
+    ladder_array = signal_branch['Particle/Particle.Ladder'].array(library="ak")
+    chip_array = signal_branch['Particle/Particle.Chip'].array(library="ak")
     
     #sort hits by frame 
-    index_1 = ak.argsort(pT_array_1, ascending = False)
+    index_1 = ak.argsort(frame_array, ascending = False)
     
-    pT_array_1 = pT_array_1[index_1]
-    eta_array_1 = eta_array_1[index_1]
-    phi_array_1 = phi_array_1[index_1]
+    frame_array = frame_array[index_1]
+    pixelx_array = pixelx_array[index_1]
+    pixely_array = pixely_array[index_1]
+    layer_array = layer_array[index_1]
+    station_array = station_array[index_1]
+    ladder_array = ladder_array[index_1]
+    chip_array = chip_array[index_1]
+    
 
-    return pT_array_1,eta_array_1,phi_array_1
+    return frame_array,pixelx_array,pixely_array,layer_array,station_array,ladder_array,chip_array
 
+###############
 
 #split the whole dataset and make labels for training and testing
-def fromiter_convert(split_ratio,pT_array_1,eta_array_1,phi_array_1):
+def fromiter_convert(split_ratio,layer_array,pixely_array,pixelx_array):
 
-    num = int(len(phi_array_1))
+    num = int(len(pixelx_array))
     mid = int(num*split_ratio)
 
     #training part
-    train_pT = awkward0.fromiter(pT_array_1[0:mid])
-    train_eta = awkward0.fromiter(eta_array_1[0:mid])
-    train_phi = awkward0.fromiter(phi_array_1[0:mid])
+    train_pT = awkward0.fromiter(layer_array[0:mid])
+    train_eta = awkward0.fromiter(pixely_array[0:mid])
+    train_phi = awkward0.fromiter(pixelx_array[0:mid])
 
     #testing part
-    test_pT = awkward0.fromiter(pT_array_1[mid:num])
-    test_eta = awkward0.fromiter(eta_array_1[mid:num])
-    test_phi = awkward0.fromiter(phi_array_1[mid:num])
+    test_pT = awkward0.fromiter(layer_array[mid:num])
+    test_eta = awkward0.fromiter(pixely_array[mid:num])
+    test_phi = awkward0.fromiter(pixelx_array[mid:num])
 
 
     return train_pT,train_eta,train_phi,test_pT,test_phi,test_eta
@@ -119,15 +130,15 @@ def MakeDataset():
     pT = 0
     split_ratio = 0.75
     signal_dir = "inputs/delphes_output.root"
-    
+
 
     print ("Reading dataset...")
     # Dataset(signal_dir,bkg_dir,eta_std)
-    pT_array_1,eta_array_1,phi_array_1 = Dataset(signal_dir)
+    layer_array,pixely_array,pixelx_array = Dataset(signal_dir)
     
     print ("Converting...")
-    # fromiter_convert(split_ratio,pT_array_1,eta_array_1,phi_array_1,pT_array_0,eta_array_0,phi_array_0)
-    train_pT,train_eta,train_phi,test_pT,test_phi,test_eta = fromiter_convert(split_ratio,pT_array_1,eta_array_1,phi_array_1)
+    # fromiter_convert(split_ratio,layer_array,pixely_array,pixelx_array,pT_array_0,eta_array_0,phi_array_0)
+    train_pT,train_eta,train_phi,test_pT,test_phi,test_eta = fromiter_convert(split_ratio,layer_array,pixely_array,pixelx_array)
 
     print ("Padding...")
     # padding(phi,eta,pT,train_pT,train_eta,train_phi,test_pT,test_phi,test_eta)
