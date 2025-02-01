6ed9bee0f9d746d8b47df6836c526f34d516be80
root <root@NickLaptop.>
2025-02-01 19:15:17 +0000

    trirec investigation

--- N Codes/extract_hits_optimised.py
+++ N Codes/extract_hits_optimised.py
@@ -74,15 +74,15 @@ def HitsInFrame(frame_number, mu3eTrame):
 #######################################################################################################
 
 # Create directory for file saving
-directory = "/root/Mu3eProject/WorkingVersion/Mu3eProject/DataFilesV5.3/signal1_97_32652" #NOTE: currently this needs to be changed each time
+directory = "/root/Mu3eProject/WorkingVersion/Mu3eProject/DataFilesV5.3/signal1_99_32652" #NOTE: currently this needs to be changed each time
 if not os.path.exists(directory):
     os.makedirs(directory)
-file_name = "hits_data_signal1_97_32652_with_mcinfo.csv"
+file_name = "hits_data_signal1_99_32652_with_mcinfo_tid_sorted.csv"
 if os.path.exists(file_name): # Deletes old version of file if present
     os.remove(file_name)
 
 # Inputting a file and state which one testing
-root_file_path = "/root/Mu3eProject/RawData/v5.3/signal1_97_32652_execution_1_run_num_716703_sort.root"
+root_file_path = "/root/Mu3eProject/RawData/v5.3/signal1_99_32652_execution_1_run_num_561343_sort.root"
 print("Test with the file:", root_file_path) 
 print()
 
@@ -109,6 +109,11 @@ for frame_number in frame_numbers:
     print("Frame number:", frame_number)
     frame_hits = HitsInFrame(frame_number, mu3eTree)
     all_hits.extend(frame_hits)
+
+
+# Sort hits by frame number and then by tid within each frame
+all_hits.sort(key=lambda x: (x['frameNumber'], x['tid'],x['hid']))
+
     
 
 # Converting array to Panda, and then saving as CSV 
